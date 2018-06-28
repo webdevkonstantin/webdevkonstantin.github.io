e6160d62202b50e8136331b6b898af2a3901918f
webdevkonstantin <36104984+webdevkonstantin@users.noreply.github.com>
2018-06-28 16:44:41 +0300

    Add files via upload
    Загрузка групп из JSON файла динамически

--- projects/football-fever/js/main.js
+++ projects/football-fever/js/main.js
@@ -1,30 +1,68 @@
 $(document).ready(function() {
-     //$.getJSON('/issueData', function (data) {
-    //$.getJSON('http://www.mocky.io/v2/5b0ea1ef320000cb2fc19b7e', function (data) {
-    //  $.getJSON('js/issues.json', function (data) {
-    //     console.log('data', data);
-    //
-    //     for(var i=0; i<data.issues.length; i++) {
-    //         var button = '<button type=\"button\" class=\"btn btn-primary\" onclick=sendIssue("' + data.issues[i].name + '");>Отправить</button>';
-    //         var status = '';
-    //
-    //         if (data.issues[i].status == '100%') {
-    //             button = '';
-    //             status = 'class="table-success"';
-    //         }
-    //
-    //         $('#issues').append('<tr '+ status +'><td>' + data.issues[i].name +
-    //             '</td><td>' + data.issues[i].name +
-    //             '</td><td>' + data.issues[i].status +
-    //             '</td><td>' + data.issues[i].dateCreated +
-    //             '</td><td>' + data.issues[i].dateSend +
-    //             '</td><td>' + data.issues[i].Sender +
-    //             '</td><td>' + button + '</td></tr>');
-    //     }
-    //
-    // });
+    initGroups()
 });
 
-function sendIssue(issueName) {
-    // console.log('issueName: ', issueName);
+function teamsDraw(teamsData) {
+    var teams = teamsData;
+    console.log('teams: ', teams);
+
+    var table = '';
+
+    table =
+        '<table class="table-bordered table-hover">' +
+        '    <thead>' +
+        '        <tr>' +
+        '            <th><span></span></th>' +
+        '            <th><span>Команда</span></th>' +
+        '            <th><span>Очки</span></th>' +
+        '            <th><span title="Разница забитых и пропущенных мечей">РМ</span></th>' +
+        '        </tr>' +
+        '    </thead>' +
+        '    <tbody class="card-table__tbody">';
+
+    for(var i=0; i<teams.length; i++) {
+        table+=
+            '<tr>' +
+            '    <td>' +
+            '        <img class="card-table__flag" src="'+ teams[i].flag +'">' +
+            '    </td>' +
+            '    <td>'+ teams[i].name +'</td>' +
+            '    <td class="text-center">'+ teams[i].points +'</td>' +
+            '    <td class="text-center">'+ teams[i].gd +'</td>' +
+            '</tr>';
+    }
+
+    table += '</tbody></table>';
+
+    return table;
+}
+
+function groupsDraw(groupData) {
+    var groups = groupData.groups;
+
+    if(groups != 'undefined' && groups.length) {
+        for(var i=0; i<groups.length; i++) {
+            $('.main-groups')
+                .append(
+                    '<div id="group-'+ groups[i].id +'" class="card card-group">' +
+                    '    <div class="card-header">'+ groups[i].name +'</div>' +
+                    '    <div class="card-table">' +
+                    teamsDraw(groups[i].teams)+
+                    '    </div>' +
+                    '</div>'
+                );
+        }
+    }
+}
+
+//Получаем данные групп в формате JSON
+function getGroupsData(){
+    //Тестовый JSON с данными о группах
+    $.getJSON('js/groups.json', function (response) {
+        groupsDraw(response);
+    });
+}
+
+function initGroups() {
+    getGroupsData();
 }
